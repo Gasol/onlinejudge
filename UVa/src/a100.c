2c35cacb3d10e360fa3df11b9e7fd47e8ffd7c16
Gasol Wu <gasol.wu@gmail.com>
2012-07-05 01:34:47 +0800

    Solve 100 - The 3n + 1 problem
    * Initial autotools
    * Use check, A unit testing framework for C

---
+++ UVa/src/a100.c
@@ -0,0 +1,55 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"a100.h"
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int j = 0;
+
+	while (scanf("%d %d", &i, &j) > 0) {
+		printf("%d %d %d\n", i, j, max_cycle(i, j));
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int max_cycle(int i, int j)
+{
+	int begin = 0;
+	int end = 0;
+	if (i > j) {
+		begin = j;
+		end = i;
+	} else {
+		begin = i;
+		end = j;
+	}
+
+	int max = 0;
+	int k = 0;
+	for (k = begin; k < end + 1; k++) {
+		int cycle = cycle_count(k);
+		if (cycle > max) {
+			max = cycle;
+		}
+	}
+
+	return max;
+}
+
+int cycle_count(int num)
+{
+	int cycle = 1;
+
+	while (num != 1) {
+		if (num % 2) {
+			num = 3 * num + 1;
+		} else {
+			num = num / 2;
+		}
+		cycle++;
+	}
+
+	return cycle;
+}
---
+++ UVa/src/a100.h
@@ -0,0 +1,8 @@
+#ifndef UVA_SRC_A100_H_
+#define UVA_SRC_A100_H_
+
+int cycle_count(int num);
+
+int max_cycle(int i, int j);
+
+#endif  // UVA_SRC_A100_H_
